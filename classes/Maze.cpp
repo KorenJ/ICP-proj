3fc959ad06ed93feb7d3a22ddc9ac19bc67dd7ff
KorenJ <korinja.2021@seznam.cz>
2023-04-26 15:20:16 +0200

    fix: odsazení

--- classes/Maze.cpp
+++ classes/Maze.cpp
@@ -8,99 +8,93 @@
 #include "../headers/Field.hpp"
 #include "../headers/Maze.hpp"
 
-        bool Maze::startReading(int rows, int cols){
-            this->rows = rows+2;
-            this->cols = cols+2;
-            this->current_row = 1;
+bool Maze::startReading(int rows, int cols){
+    this->rows = rows+2;
+    this->cols = cols+2;
+    this->current_row = 1;
 
-            try{
-                this->field = new Field**[this->rows];
-                for (int i = 0; i < this->rows; ++i)
-                    this->field[i] = new Field*[this->cols];
+    try{
+        this->field = new Field**[this->rows];
+        for (int i = 0; i < this->rows; ++i)
+            this->field[i] = new Field*[this->cols];
 
-                for (int i = 0; i < this->cols; i++){
-                    this->field[0][i] = new Field(0, i, *this);
-                }
-            }
-            catch (std::exception& e){
-                std::cerr << "Chyba: " << e.what() << std::endl;
+        for (int i = 0; i < this->cols; i++)
+            this->field[0][i] = new Field(0, i, *this);
+    }
+    catch (std::exception& e){
+        std::cerr << "Chyba: " << e.what() << std::endl;
+        return false;
+    }
+
+    return true;            
+}
+
+bool Maze::processLine(std::string line){
+    const int lenght = line.length();
+    if (lenght != this->cols-2){
+        return false;
+    }
+    char *parsed_line = new char[lenght+1];
+    strcpy(parsed_line, line.c_str());
+
+    this->field[this->current_row][0] = new Field(this->current_row, 0, *this);
+    for (int i = 1; i < lenght+1; i++){
+        switch(parsed_line[i-1]){
+            // prázdná cesta
+            case '.':
+                this->field[this->current_row][i] = new Field(this->current_row, i, *this);
+                this->field[this->current_row][i]->setPath();
+                break;
+            // zeď
+            case 'X':
+                this->field[this->current_row][i] = new Field(this->current_row, i, *this);
+                break;
+            // cesta s duchem
+            case 'G':
+                this->field[this->current_row][i] = new Field(this->current_row, i, *this);
+                this->field[this->current_row][i]->setPath();
+                break;
+            // cesta s pacmanem
+            case 'S':
+                this->field[this->current_row][i] = new Field(this->current_row, i, *this);
+                this->field[this->current_row][i]->setPath();
+                break;
+            // cesta s cílem
+            case 'T':
+                this->field[this->current_row][i] = new Field(this->current_row, i, *this);
+                this->field[this->current_row][i]->setPath();
+                break;
+            default:
+                std::cerr << "Neznámé políčko\n";
                 return false;
-            }
-
-            return true;            
         }
+    }
+    this->field[this->current_row][this->cols-1] = new Field(this->current_row, 0, *this);
+    this->current_row++;
+    return true;
+}
 
-        bool Maze::processLine(std::string line){
-            const int lenght = line.length();
-            if (lenght != this->cols-2){
-                return false;
-            }
-            char *parsed_line = new char[lenght+1];
-            strcpy(parsed_line, line.c_str());
+bool Maze::stopReading(){
+    for (int i = 0; i < this->cols; i++){            
+        this->field[this->rows-1][i] = new Field(this->rows-1, i, *this);
+    }
+    return true;
+}
 
-            this->field[this->current_row][0] = new Field(this->current_row, 0, *this);
+Maze Maze::createMaze(){
+    return *this;
+}
 
-            for (int i = 1; i < lenght+1; i++){
-                switch(parsed_line[i-1]){
-                    // prázdná cesta
-                    case '.':
-                        this->field[this->current_row][i] = new Field(this->current_row, i, *this);
-                        this->field[this->current_row][i]->setPath();
-                        break;
-                    // zeď
-                    case 'X':
-                        this->field[this->current_row][i] = new Field(this->current_row, i, *this);
-                        break;
-                    // cesta s duchem
-                    case 'G':
-                        this->field[this->current_row][i] = new Field(this->current_row, i, *this);
-                        this->field[this->current_row][i]->setPath();
-                        break;
-                    // cesta s pacmanem
-                    case 'S':
-                        this->field[this->current_row][i] = new Field(this->current_row, i, *this);
-                        this->field[this->current_row][i]->setPath();
-                        break;
-                    // cesta s cílem
-                    case 'T':
-                        this->field[this->current_row][i] = new Field(this->current_row, i, *this);
-                        this->field[this->current_row][i]->setPath();
-                        break;
-                    default:
-                        std::cerr << "Neznámé políčko\n";
-                        return false;
-                }
-            }
-
-            this->field[this->current_row][this->cols-1] = new Field(this->current_row, 0, *this);
-
-            this->current_row++;
-
-            return true;
-
+void Maze::clearMaze(){
+    for (int i = 0; i < this->rows; ++i){
+        for (int j = 0; j < this->cols; ++j){
+            delete [] this->field[i][j];
         }
+        delete [] this->field[i];
+    }
+    delete [] this->field;                
+}
 
-        bool Maze::stopReading(){
-            for (int i = 0; i < this->cols; i++){
-                this->field[this->rows-1][i] = new Field(this->rows-1, i, *this);
-            }
-            return true;
-        }
-
-        Maze Maze::createMaze(){
-            return *this;
-        }
-
-        void Maze::clearMaze(){
-            for (int i = 0; i < this->rows; ++i){
-                for (int j = 0; j < this->cols; ++j){
-                    delete [] this->field[i][j];
-                }
-                delete [] this->field[i];
-            }
-            delete [] this->field;                
-        }
-
-        Field Maze::getField(int row, int col){
-            return *this->field[row][col];
-        }
+Field Maze::getField(int row, int col){
+    return *this->field[row][col];
+}
