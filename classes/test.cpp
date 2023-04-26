8d517a4368d5c9bd41239a404f8ce8bba70f875b
KorenJ <korinja.2021@seznam.cz>
2023-04-26 14:45:55 +0200

    fix: smazání binárky

--- classes/test.cpp
+++ classes/test.cpp
@@ -10,15 +10,15 @@ int main(){
 
     Maze *m = new Maze();
     m->startReading(2,2);
-    m->processLine("..");
+    m->processLine("X.");
     m->processLine("..");
     m->stopReading();
     Maze maze = m->createMaze();
 
-    Field f = maze.getField(1,1);
+    Field f = maze.getField(1,2);
     if (f.canMove())
         std::cout << "SUCCESS\n";
-    Field f1 = maze.getField(0,1);
+    Field f1 = maze.getField(1,1);
     if (!f1.canMove())
         std::cout << "SUCCESS\n";
 
