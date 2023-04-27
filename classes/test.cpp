8cb98bd13bb599dea311d1b285c5c41088fadaba
KorenJ <korinja.2021@seznam.cz>
2023-04-27 16:29:21 +0200

    feat: úklid objektů pole

--- classes/test.cpp
+++ classes/test.cpp
@@ -10,15 +10,15 @@ int main(){
     std::ifstream file;
     file.open("../save/map.txt");
     Maze *m = new Maze();
+    int rows;
+    int cols;
 
     if (file.is_open()){
         std::string str_rows;
         file >> str_rows;
-        int rows;
         rows = stoi(str_rows);
         std::string str_cols;
         file >> str_cols;
-        int cols;
         cols = stoi(str_cols);
 
         std::string inputstring;
@@ -33,13 +33,14 @@ int main(){
             }
             m->processLine(inputstring);
         }
+        
+        m->stopReading();
     }
     else{
         std::cerr << "Chyba načtení souboru\n";
         return 1;
     }
 
-    m->stopReading();
     Maze maze = m->createMaze();
 
     Field *f = maze.getField(1,1);
@@ -78,6 +79,17 @@ int main(){
     if (!f->isEmpty())
         std::cout << "SUCCESS4.8 - PACMAN DIDN'T MOVE\n";
 
+    //uklid
+    for (int i = 1; i <= rows; i++){
+        for (int j = 1; j <= cols; j++){
+            Field *tmp = maze.getField(i,j);
+            if (!tmp->isEmpty()){
+                MazeObject *object = tmp->get();
+                tmp->remove(object);
+                delete object;
+            }
+        }
+    }
     maze.clearMaze();
     file.close();
 
